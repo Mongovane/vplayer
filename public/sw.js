71cc2a0cefb145f17f7ee5994752fccb84d48ccf
VPlayer <dev@vplayer.local>
2026-09-04 09:53:15 +0800

    Fix add play5

--- public/sw.js
+++ public/sw.js
@@ -17,7 +17,7 @@
  *     because Range requests and a Cache Storage entry do not mix
  */
 
-const CACHE = 'vplayer-runtime-v63';
+const CACHE = 'vplayer-runtime-v64';
 const SHELL = [
   '/',
   '/index.html',
